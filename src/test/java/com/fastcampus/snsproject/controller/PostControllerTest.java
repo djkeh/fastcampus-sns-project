326a411a35f7d6afc12224ee38a6129558a7797c
donghyunlee <skdonghyunlee@gmail.com>
2023-02-06 20:36:14 +0900

    use this template -> fork

--- src/test/java/com/fastcampus/snsproject/controller/PostControllerTest.java
+++ src/test/java/com/fastcampus/snsproject/controller/PostControllerTest.java
@@ -21,6 +21,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
