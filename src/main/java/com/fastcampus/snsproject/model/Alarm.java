2289d7608044760d7837a6bc7f47de6dbb4f7de2
DESKTOP-1ODPVJJ\bae <joonguk90@gmail.com>
2023-02-12 23:50:57 +0900

    과제 수행 내역 commit

---
+++ src/main/java/com/fastcampus/snsproject/model/Alarm.java
@@ -0,0 +1,31 @@
+package com.fastcampus.snsproject.model;
+
+import com.fastcampus.snsproject.model.entity.AlarmEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.Timestamp;
+
+@Slf4j
+@Getter
+@AllArgsConstructor
+public class Alarm {
+    private Integer id;
+    private AlarmType alarmType;
+    private AlarmArgs Args;
+    private Timestamp registeredAt;
+    private Timestamp updatedAt;
+    private Timestamp deletedAt;
+
+    public static Alarm fromEntity(AlarmEntity entity) {
+        return new Alarm(
+                entity.getId(),
+                entity.getAlarmType(),
+                entity.getArgs(),
+                entity.getRegisteredAt(),
+                entity.getUpdatedAt(),
+                entity.getDeletedAt()
+        );
+    }
+}
---
+++ src/main/java/com/fastcampus/snsproject/model/AlarmArgs.java
@@ -0,0 +1,11 @@
+package com.fastcampus.snsproject.model;
+
+import lombok.Data;
+
+@Data
+public class AlarmArgs {
+
+    // 알람을 발생시킨 사람
+    private Integer fromUserId;
+    private Integer targetId;
+}
