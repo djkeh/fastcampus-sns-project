8d17e7c86e09e00fa1365cf72407cf82d5b0b579
sb2ch <sb2chun@gmail.com>
2023-02-13 02:01:36 +0900

    과제 제출 fin.
     - 기존 Use this template으로 개발을 진행하였고, fork방식으로 새로 수정해서 올립니다.
     - 헤로쿠 대응 ㅇ나되어있음.
     - postgre 대신 mybatis로 진행 (초기 단계에 Postgre 이슈가 많았어서 동일 ORM 방식이라 문제 없을 것으로 생각하였습니다.)
    
     - 기타: 현재 Postman, test 등에서는 정상작동하나 화면에서는 열리지 않습니다...
                 해당 부분에 대한 원인을 찾지 못하여 리뷰 부탁드립니다. 감사합니다.

--- src/main/java/com/fastcampus/snsproject/SnsProjectApplication.java
+++ src/main/java/com/fastcampus/snsproject/SnsProjectApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FastcampusSnsProjectApplication {
+public class SnsProjectApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FastcampusSnsProjectApplication.class, args);
+        SpringApplication.run(SnsProjectApplication.class, args);
     }
 
 }
---
+++ src/main/java/com/fastcampus/snsproject/repository/AlarmEntityRepository.java
@@ -0,0 +1,11 @@
+package com.fastcampus.snsproject.repository;
+
+import com.fastcampus.snsproject.model.entity.AlarmEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlarmEntityRepository extends JpaRepository<AlarmEntity, Long> {
+
+    Page<AlarmEntity> findAllByUserId(Integer userId, Pageable pageable);
+}
