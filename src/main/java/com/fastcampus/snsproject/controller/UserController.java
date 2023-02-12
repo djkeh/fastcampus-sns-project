2ab9bd88b8c112be7ef17b59963f5ca45fdaa615
sun-office <SUN11@sk.com>
2023-02-12 23:38:57 +0900

    Updated
    Back-end
    Part 8. Ch02.

---
+++ src/main/java/com/fastcampus/snsproject/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.fastcampus.snsproject.controller;
+
+import com.fastcampus.snsproject.controller.request.UserJoinRequest;
+import com.fastcampus.snsproject.controller.request.UserLoginRequest;
+import com.fastcampus.snsproject.controller.response.Response;
+import com.fastcampus.snsproject.controller.response.UserJoinResponse;
+import com.fastcampus.snsproject.controller.response.UserLoginResponse;
+import com.fastcampus.snsproject.model.User;
+import com.fastcampus.snsproject.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public Response join(@RequestBody UserJoinRequest request) {
+        User user = userService.join(request.getName(), request.getPassword());
+        return Response.success(UserJoinResponse.fromUser(user));
+    }
+
+    @PostMapping("/login")
+    public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
+        String token = userService.login(request.getName(), request.getPassword());
+        return Response.success(new UserLoginResponse(token));
+    }
+}
