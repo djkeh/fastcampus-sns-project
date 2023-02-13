62c14d19bb74a8a11985eb8803d0674d5c5b7615
DAHEECHOI1208 <shoodol00@gmail.com>
2023-02-13 16:00:33 +0900

    Update FastcampusSnsProjectApplication.java

--- src/main/java/com/fastcampus/snsproject/FastcampusSnsProjectApplication.java
+++ src/main/java/com/fastcampus/snsproject/FastcampusSnsProjectApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FastcampusSnsProjectApplication {
 
+    //TODO
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSnsProjectApplication.class, args);
     }
